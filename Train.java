8210f0c62972f012d8d8def3947bab1c45af887b
Akash-Kamaliya <akashkamaliya112@gmail.com>
2025-02-04 07:00:47 +0530

    add some file and start project

---
+++ Train.java
@@ -0,0 +1,37 @@
+class Passanger {
+	String name;
+	int mobileNumber;
+	String emailId;
+	int age;
+	int dateOfBirth;
+}
+class Ticket {
+	int PNRnum;
+	String trainName;
+	String startLocation;
+	String endLocation;
+	String name;
+	int price;
+}
+class Payment {
+	String cardType;
+	int cardNumber;
+	String name;
+	int price;
+	String cancelPayment;
+}
+class Coaches {
+	int numberOfSeat;
+	String typeOfCoach;
+}
+class Extra {
+	String personWithDisability;
+	String editProfile;
+}
+
+public class Train {
+	public static void main(String [] args){
+		int trainNumber;
+		String trianName;
+	}
+}
